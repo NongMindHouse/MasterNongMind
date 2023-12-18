c72e17e6f8406aacb6e0b0105f791edcc5988e6d
CH0KUN <chokunworking4713@gmail.com>
2023-12-19 00:08:15 +0700

    ✨ add progress bar

--- grid_search.py
+++ grid_search.py
@@ -13,16 +13,19 @@ if __name__ == '__main__':
     print('Using ', c_file_path)
 
     # Loop for Grid Search 
-    for population_length  in tqdm(range(50,550,50)):
-        for max_gen in range(50,550,50):
-            for mutation_rate in np.arange(0.05, 0.55, 0.05):
-                params = [
-                    color_length,        # CODE_LENGTH Fixed
-                    colors_num,        # COLORS Fixed
-                    max_guess,       # MAX_GUESS Fixed
-                    str(population_length),  # POPULATION_LENGTH  
-                    str(max_gen),                 # MAX_GEN  
-                    str(mutation_rate),         # MUTATION_RATE
-                ]
-                benchmarkExceution(c_file_path, export_folder, params, test_size)
-                
+    total_iterations = 1000
+    # Create a single progress bar for all three loops
+    with tqdm(total=total_iterations, desc="🐥 Overall Status") as pbar_outer:
+        for population_length  in range(50,550,50):
+            for max_gen in range(50,550,50):
+                for mutation_rate in np.arange(0.00, 0.55, 0.05):
+                    params = [
+                        color_length,        # CODE_LENGTH Fixed
+                        colors_num,        # COLORS Fixed
+                        max_guess,       # MAX_GUESS Fixed
+                        str(population_length),  # POPULATION_LENGTH  
+                        str(max_gen),                 # MAX_GEN  
+                        str(mutation_rate),         # MUTATION_RATE
+                    ]
+                    benchmarkExceution(c_file_path, export_folder, params, test_size)
+                    pbar_outer.update(1)
