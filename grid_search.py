371d1a236e53c94ea99cbe1f7968ad9e23457cd6
CH0KUN <chokunworking4713@gmail.com>
2023-12-18 23:51:12 +0700

    🎨 rearrange variable declaration

--- grid_search.py
+++ grid_search.py
@@ -4,19 +4,22 @@ import numpy as np
 
 if __name__ == '__main__':
     c_file_path = "mm-ga-pretty.c"
-    export_folder = "results-4-6"
+    color_length = '5'
+    colors_num = '8'
+    max_guess = '10'
+    export_folder = f"results-{color_length}-{colors_num}"
     test_size = 1000
     print("Strat Grid Search")
     print('Using ', c_file_path)
 
     # Loop for Grid Search 
-    for population_length  in tqdm(range(50,100,50)):
-        for max_gen in range(50,100,50):
-            for mutation_rate in np.arange(0.05, 0.15, 0.05):
+    for population_length  in tqdm(range(50,550,50)):
+        for max_gen in range(50,550,50):
+            for mutation_rate in np.arange(0.05, 0.55, 0.05):
                 params = [
-                    '4',        # CODE_LENGTH Fixed
-                    '6',        # COLORS Fixed
-                    '10',       # MAX_GUESS Fixed
+                    color_length,        # CODE_LENGTH Fixed
+                    colors_num,        # COLORS Fixed
+                    max_guess,       # MAX_GUESS Fixed
                     str(population_length),  # POPULATION_LENGTH  
                     str(max_gen),                 # MAX_GEN  
                     str(mutation_rate),         # MUTATION_RATE
